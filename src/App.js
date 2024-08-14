9ed8c213d989606523bedae95045676ef72f8859
fabian <fabianrk@gmail.com>
2024-08-15 00:54:45 +0300

    go '/' if wrong url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
 import PublicProfile from './PublicProfile';
@@ -8,9 +8,10 @@ import EventPage from './EventPage';
 import LoginTwoFactorAuth from './Login-2FA';
 import SignUpTwoFactorAuth from './SignUp-2FA';
 import Cookies from 'js-cookie';
-import EditProfile from './EditProfile';
+import MyProfile from './MyProfile';
 import ServicesPage from './ServicesPage';
 import EventonLandingPage from './EventonLandingPage';
+import EditProfile from './EditProfile';
 
 const App = () => {
   const token = Cookies.get('userToken');
@@ -20,13 +21,16 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signup-2fa" element={token ? <SignUpTwoFactorAuth /> : <Login />} />
-        <Route path="/profile/:id" element= {<PublicProfile />}/>
-        <Route path="/my-profile" element={<EditProfile />} />
-        <Route path="/" element={<EventonLandingPage/>}/>
-        <Route path="/login" element={token ? <EventPage /> : <Login />} />
+        <Route path="/signup-2fa" element={<SignUpTwoFactorAuth />} />
+        <Route path="/login-2fa" element={<LoginTwoFactorAuth />} />
+        <Route path="/profile/:id" element={<PublicProfile />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/" element={<EventonLandingPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/events" element={<EventPage />} />
         <Route path="/services" element={<ServicesPage />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="*" element={<Navigate to ='/' />}/>
       </Routes>
     </Router>
   );
